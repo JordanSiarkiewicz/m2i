60b8fa157a025497aa8b4112477529df052bd21f
JordanSiarkiewicz <jordan.siarkiewicz@gmail.com>
2024-09-10 14:22:37 +0200

    Ajout exo 2 Spring

---
+++ Spring_Exo02/src/test/java/org/example/spring_exo02/SpringExo02ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spring_exo02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExo02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
